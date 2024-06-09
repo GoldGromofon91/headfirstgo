a18e08ce4bb5a74f5c6a9d30507d913eed471b06
Nikita Savchenko <58365071+GoldGromofon91@users.noreply.github.com>
2024-06-09 12:26:12 +0300

    Update keyboard.go

--- keyboard.go
+++ keyboard.go
@@ -1,1 +1,42 @@
+/*
+	Пакет оптимизации кода в каталоге Desktop/go/1.Basic/
+*/
 
+package keyboard
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func GetFloat() (float64, error) {
+	// Функция получения и преобразования строки в число с типом float64 полученой из потока Input
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
+
+func GetInt() (int, error) {
+	// Функция получения и преобразования строки в число с типом int полученой из потока Input
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	input = strings.TrimSpace(input)
+	number, err := strconv.Atoi(input)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
